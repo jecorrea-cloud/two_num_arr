600c3ffb8a5aa3bea1d0ed26cab74b4b4de45449
joaquin <jecorrea@protonmail.com>
2022-01-03 20:52:05 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function twoNumberSum(array, targetSum) {
+  // Write your code here.
+	// O(n^2) time | O(1) space 
+	for(let i =0; i<array.length-1; i++){
+		const firstNum = array[i];
+		for(let j= i + 1; j<array.length; j++){
+			const secondNum = array[j]
+			if(firstNum + secondNum === targetSum) return [firstNum, secondNum]
+    }
+	}
+	return [];
+}
